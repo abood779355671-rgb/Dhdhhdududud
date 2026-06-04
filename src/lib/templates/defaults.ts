99e732d290b33a1d73bc188ce6b8608a900b70dc
abood779355671-rgb <abood779355671@gmail.com>
2026-06-04 03:38:09 +0300

    upload src/lib/templates/defaults.ts

---
+++ src/lib/templates/defaults.ts
@@ -0,0 +1,13 @@
+import { TemplateContent } from './types';
+
+export const DEFAULT_CONTENT: TemplateContent = {
+  brideName: 'العروس',
+  groomName: 'العريس',
+  weddingDate: '٢٠ من ربيع الأول ١٤٤٧ هـ',
+  venueName: 'قاعة الأفراح الفاخرة',
+  venueAddress: 'الرياض — المملكة العربية السعودية',
+  message: 'يتشرفون بدعوتكم لحضور حفل زفافهم ومشاركتهم أجمل لحظات حياتهم',
+  parents: 'يتشرف آل العريس وآل العروس بدعوتكم',
+  arabicDate: '',
+  time: 'بعد صلاة العشاء'
+};
