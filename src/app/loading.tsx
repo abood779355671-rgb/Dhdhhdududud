dd32f9f19856a513a563101fb292c406c71245d4
abood779355671-rgb <abood779355671@gmail.com>
2026-06-04 03:36:04 +0300

    upload src/app/loading.tsx

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import LoadingSpinner from '@/components/LoadingSpinner';
+
+  export default function Loading() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-navy-950">
+        <LoadingSpinner size="lg" />
+      </div>
+    );
+  }
+  
